24c7ba1d412141b205729adb929a4d214fea3edf
Raffaele <88720362+RaffaeleBini@users.noreply.github.com>
2024-04-11 13:30:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,44 +26,6 @@ function decrementa() {
 incrementaButton.addEventListener("click", incrementa);
 decrementaButton.addEventListener("click", decrementa);
 
-// Funzionalità aggiuntive
-
-// Imposta un limite minimo al contatore
-const limiteMinimo = -10;
-
-// Funzione per disabilitare il pulsante decrementa quando il contatore raggiunge il limite minimo
-function disabilitaDecrementa() {
-  if (contatore === limiteMinimo) {
-    decrementaButton.disabled = true;
-  }
-}
-
-// Funzione per abilitare il pulsante decrementa quando il contatore supera il limite minimo
-function abilitaDecrementa() {
-  if (contatore > limiteMinimo) {
-    decrementaButton.disabled = false;
-  }
-}
-
-// Imposta un limite massimo al contatore
-const limiteMassimo = 100;
-
-// Funzione per disabilitare il pulsante incrementa quando il contatore raggiunge il limite massimo
-function disabilitaIncrementa() {
-  if (contatore === limiteMassimo) {
-    incrementaButton.disabled = true;
-  }
-}
-
-// Funzione per abilitare il pulsante incrementa quando il contatore scende sotto il limite massimo
-function abilitaIncrementa() {
-  if (contatore < limiteMassimo) {
-    incrementaButton.disabled = false;
-  }
-}
-
-// Aggiunta dei controlli per i limiti
-disabilitaDecrementa();
 
 // Aggiornamento del contatore al caricamento della pagina
 aggiornaContatore();
